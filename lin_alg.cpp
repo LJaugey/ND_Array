5964438820930439504b930d78b34eaf3211e36d
LJaugey <louis.jaugey@epfl.ch>
2023-08-18 16:56:35 +0200

    Optimzed linear algebra operations

--- lin_alg.cpp
+++ lin_alg.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 #include "linear_algebra.hpp"
+#include <chrono>
 
 
 using namespace std;
 
+#define SIZE 2500
+
 int main()
 {
-    Matrix<4,4> A;
-    A.fill(0.5);
-    Matrix<4,4> B;
-    B.fill(0.5);
+    Matrix<SIZE,SIZE> A;
+    A.fill(0.1);
+    Matrix<SIZE,SIZE> B;
+    B.fill(0.01);
 
-    Matrix<4,4> C = A*B;
+    auto start = std::chrono::high_resolution_clock::now();
+    Matrix<SIZE,SIZE> C = A*B;
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    cout<<endl<<"Code ran in "<<(double)duration.count()/1000.0<<" seconds"<<endl<<endl;
+    
 
-    cout<<A<<endl<<endl;
-    cout<<B<<endl<<endl;
-    cout<<C<<endl<<endl;
+    /*
+    Matrix<12*SIZE,12*SIZE> M;
 
-    cout<<endl<<endl;
+    Vector<12*SIZE> V;
 
-    Vector<4> V;
-    V.fill(0.5);
+    auto start = std::chrono::high_resolution_clock::now();
+    Vector<12*SIZE> res = M*V;
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    cout<<endl<<"Code ran in "<<(double)duration.count()/1000.0<<" seconds"<<endl<<endl;
+    */
 
-    Vector<4> res = A*V;
+    /*
+    Vector<SIZE*SIZE*80> A;
+    A.fill(1.0/SIZE);
+    Vector<SIZE*SIZE*80> B;
+    B.fill(1.0/SIZE);
 
-    cout<<res<<endl<<endl;
-    cout<<endl<<endl;
-
-    cout<<V*res<<endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    double res = A*B;
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    cout<<endl<<"Code ran in "<<(double)duration.count()/1000.0<<" seconds"<<endl<<endl;
+    */
 
     return 0;
 }
--- linear_algebra.hpp
+++ linear_algebra.hpp
@@ -1,34 +1,186 @@
-#ifndef LINEAR_ALGEMBRA_HPP
-#define LINEAR_ALGEMBRA_HPP
+#ifndef LINEAR_ALGEBRA_HPP
+#define LINEAR_ALGEBRA_HPP
 
 #include "ND_array.hpp"
 
+#define TILE 32
 
 template<size_t dim1,size_t dim2>
 class Matrix: public Array<dim1,dim2>
 {};
+
+
 
 template<size_t dim>
 class Vector: public Array<dim>
 {};
 
 
+template<size_t dim>
+Matrix<dim,dim> diag(double val)
+{
+    Matrix<dim,dim> result;
+
+    for(size_t i=0; i<dim; i++)
+    {
+        result(i,i) = val;
+    }
+
+    return result
+}
+template<size_t dim>
+Matrix<dim,dim> diag(Vector<dim> V)
+{
+    Matrix<dim,dim> result;
+
+    for(size_t i=0; i<dim; i++)
+    {
+        result(i,i) = V(i);
+    }
+
+    return result
+}
+
+
 template<size_t dim1,size_t common_dim,size_t other_dim2>
 Matrix<dim1,other_dim2> operator*(const Matrix<dim1,common_dim>& M1, const Matrix<common_dim,other_dim2>& M2)
 {
     Matrix<dim1,other_dim2> result;
-
-    for(size_t i=0; i<dim1; i++)
+    
+    if constexpr(dim1>TILE && common_dim>TILE)
     {
-        for(size_t j=0; j<other_dim2; j++)
+        constexpr size_t last_TILE_I = ((dim1-1)/TILE);
+        constexpr size_t last_TILE_K = ((common_dim-1)/TILE);
+
+        #pragma omp parallel for collapse(2) if(M1.length>PAR_SIZE)
+        for(size_t I=0; I<last_TILE_I*TILE; I+=TILE)
         {
-            for(size_t k=0; k<common_dim; k++)
+            for(size_t K=0; K<last_TILE_K*TILE; K+=TILE)
             {
-                result(i,j) += M1(i,k)*M2(k,j);
+                for(size_t i=I; i<I+TILE; i+=2)
+                {
+                    for(size_t k=K; k<K+TILE; k+=2)
+                    {
+                        for(size_t j=0; j<other_dim2; j++)
+                        {
+                            result(i+0,j) += M1(i+0,k+0)*M2(k+0,j) + M1(i+0,k+1)*M2(k+1,j);
+                            result(i+1,j) += M1(i+1,k+0)*M2(k+0,j) + M1(i+1,k+1)*M2(k+1,j);
+                        }
+                    }
+                }
+            }
+        }
+        for(size_t I=0; I<dim1-TILE; I+=TILE)
+        {
+            for(size_t i=I; i<I+TILE; i+=2)
+            {
+                for(size_t k=last_TILE_K*TILE; k<common_dim; k++)
+                {
+                    for(size_t j=0; j<other_dim2; j++)
+                    {
+                        result(i+0,j) += M1(i+0,k+0)*M2(k+0,j);
+                        result(i+1,j) += M1(i+1,k+0)*M2(k+0,j);
+                    }
+                }
+            }
+        }
+        for(size_t K=0; K<common_dim-TILE; K+=TILE)
+        {
+            for(size_t i=last_TILE_I*TILE; i<dim1; i++)
+            {
+                for(size_t k=K; k<K+TILE; k+=2)
+                {
+                    for(size_t j=0; j<other_dim2; j++)
+                    {
+                        result(i+0,j) += M1(i+0,k+0)*M2(k+0,j) + M1(i+0,k+1)*M2(k+1,j);
+                    }
+                }
+            }
+        }
+        for(size_t i=last_TILE_I*TILE; i<dim1; i++)
+        {
+            for(size_t k=last_TILE_K*TILE; k<common_dim; k++)
+            {
+                for(size_t j=0; j<other_dim2; j++)
+                {
+                    result(i+0,j) += M1(i+0,k+0)*M2(k+0,j);
+                }
             }
         }
     }
+    else if constexpr(dim1>TILE)
+    {
+        constexpr size_t last_TILE_I = ((dim1-1)/TILE);
+        
+        #pragma omp parallel for if(M1.length>PAR_SIZE)
+        for(size_t I=0; I<last_TILE_I*TILE; I+=TILE)
+        {
+            for(size_t i=I; i<I+TILE; i+=2)
+            {
+                for(size_t k=0; k<common_dim; k++)
+                {
+                    for(size_t j=0; j<other_dim2; j++)
+                    {
+                        result(i+0,j) += M1(i+0,k+0)*M2(k+0,j);
+                        result(i+1,j) += M1(i+1,k+0)*M2(k+0,j);
+                    }
+                }
+            }
+        }
+        for(size_t i=last_TILE_I*TILE; i<dim1; i++)
+        {
+            for(size_t k=0; k<common_dim; k++)
+            {
+                for(size_t j=0; j<other_dim2; j++)
+                {
+                    result(i+0,j) += M1(i+0,k+0)*M2(k+0,j);
+                }
+            }
+        }
+    }
+    else if constexpr(common_dim>TILE)
+    {
+        constexpr size_t last_TILE_K = ((common_dim-1)/TILE);
 
+        #pragma omp parallel for if(M1.length>PAR_SIZE)
+        for(size_t K=0; K<last_TILE_K*TILE; K+=TILE)
+        {
+            for(size_t i=0; i<dim1; i++)
+            {
+                for(size_t k=K; k<K+TILE; k+=2)
+                {
+                    for(size_t j=0; j<other_dim2; j++)
+                    {
+                        result(i+0,j) += M1(i+0,k+0)*M2(k+0,j) + M1(i+0,k+1)*M2(k+1,j);
+                    }
+                }
+            }
+        }
+        for(size_t i=0; i<dim1; i++)
+        {
+            for(size_t k=last_TILE_K*TILE; k<common_dim; k++)
+            {
+                for(size_t j=0; j<other_dim2; j++)
+                {
+                    result(i+0,j) += M1(i+0,k+0)*M2(k+0,j);
+                }
+            }
+        }
+    }
+    else
+    {
+        for(size_t i=0; i<dim1; i++)
+        {
+            for(size_t k=0; k<common_dim; k++)
+            {
+                for(size_t j=0; j<other_dim2; j++)
+                {
+                    result(i,j) += M1(i,k)*M2(k,j);
+                }
+            }
+        }
+    }
+    
     return result;
 }
 
@@ -39,11 +191,39 @@ Vector<dim1> operator*(const Matrix<dim1,common_dim>& M, const Vector<common_dim
 {
     Vector<dim1> result;
 
-    for(size_t i=0; i<dim1; i++)
+    if constexpr(dim1>TILE)
     {
-        for(size_t j=0; j<common_dim; j++)
+        constexpr size_t last_TILE_I = ((dim1-1)/TILE);
+        
+        #pragma omp parallel for if(M.length>PAR_SIZE)
+        for(size_t I=0; I<last_TILE_I*TILE; I+=TILE)
         {
-            result(i) += M(i,j)*V(j);
+            for(size_t i=I; i<I+TILE; i+=2)
+            {
+                for(size_t j=0; j<common_dim; j++)
+                {
+                    result(i+0) += M(i+0,j)*V(j);
+                    result(i+1) += M(i+1,j)*V(j);
+                }
+            }
+        }
+        for(size_t i=last_TILE_I*TILE; i<dim1; i++)
+        {
+            for(size_t j=0; j<common_dim; j++)
+            {
+                result(i+0) += M(i+0,j)*V(j);
+            }
+        }
+    }
+    else
+    {
+        #pragma omp parallel for if(M.length>PAR_SIZE)
+        for(size_t i=0; i<dim1; i++)
+        {
+            for(size_t j=0; j<common_dim; j++)
+            {
+                result(i) += M(i,j)*V(j);
+            }
         }
     }
 
@@ -56,6 +236,7 @@ double operator*(const Vector<common_dim>& V1, const Vector<common_dim>& V2)
 {
     double result = 0.0;
 
+    #pragma omp parallel for reduction(+:result) if(common_dim>PAR_SIZE)
     for(size_t i=0; i<common_dim; i++)
     {
         result += V1(i)*V2(i);
